6c7b5eab87a4b47331f22cdd031fcd430578470c
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-09-15 14:12:42 +0600

    common components [creating (control panel elements)]

---
+++ src/components/common/ControlPanel/ControlPanel.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+   FaQuestion,
+   FaTimes
+} from 'react-icons';
+import cn from 'classnames';
+import s from './ControlPanel.module.css';
+
+export const CloseButton = (props) => {
+   return (
+      <div className={cn(s.button, s.CloseButton)}>
+         <FaQuestion />
+      </div>
+   );
+}
+
+export const DescriptionButton = (props) => {
+   return (
+      <div className={cn(s.button, s.DescriptionButton)}>
+         <FaTimes />
+      </div>
+   );
+}
